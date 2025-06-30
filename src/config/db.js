ca4989a260efb3207c241eb2c583616758c950ce
Hanung <hanungpr28@gmail.com>
2025-06-30 10:38:38 +0700

    dotenv

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,4 @@
-const mysql = require('mysql2/promise');
-require('dotenv').config();
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -8,7 +7,7 @@ const pool = mysql.createPool({
   database: process.env.DB_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
 });
 
 module.exports = pool;
